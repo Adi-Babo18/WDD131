b1745847e9bf1b79dd608fe79ee300a015425e55
Adi-Babo18 <clarisa.403.18@gmail.com>
2026-04-25 20:09:50 -0600

    Forget changes WEEK01

---
+++ ponder/week01/week01.js
@@ -0,0 +1,15 @@
+const PI = 3.14;
+let radius = 3;
+
+consule.log (PI);
+consule.log(radius);
+
+const password = "SolveMath1";
+
+let course = "CSE131";
+
+if (true) {
+    let student = "Ariana";
+    console.log(course);
+    console.log(student);
+}
