898605056bd4753cc7a5c897e2d4bf013896afcd
Adi-Babo18 <clarisa.403.18@gmail.com>
2026-05-02 22:02:21 -0600

    \Week-02-Prove assignment

---
+++ Week02/mission/dom-DL-Mode.js
@@ -0,0 +1,21 @@
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+
+    if (current === 'dark') {
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+
+        logo.src = "Photos/byui-logo-white.png";
+        
+    } else if (current === 'light') {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+
+        logo.src = "Photos/byui-logo-blue.webp";
+    }
+}
